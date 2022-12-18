34caff38941ab1efe71bd4c148573284a1605677
haimhamiel <haim.hamiel@gmail.com>
2022-12-18 13:50:14 +0200

    update server.js

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+
 
 require('dotenv').config()
 
@@ -23,12 +20,17 @@ const db = knex({
   }
 });
 
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors())
 
 app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) });
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
